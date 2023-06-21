79c77f3340ef1f141cbd3046f58f450e684003ee
Cheng Ziqiu <jxlpzqc@live.com>
2023-06-21 10:58:37 +0800

    feat: implement speech core.

--- TMSpeech.GUI/MainWindow.xaml.cs
+++ TMSpeech.GUI/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -36,7 +37,7 @@ namespace TMSpeech.GUI
 
         private void gridContainer_PreviewMouseMove(object sender, MouseEventArgs e)
         {
-            if (e.LeftButton== MouseButtonState.Pressed && _mouseLocation != e.GetPosition(this))
+            if (e.LeftButton == MouseButtonState.Pressed && _mouseLocation != e.GetPosition(this))
             {
                 _isDrag = true;
                 DragMove();
@@ -46,12 +47,71 @@ namespace TMSpeech.GUI
 
         private void gridContainer_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
-            if(_isDrag)
+            if (_isDrag)
             {
                 _isDrag = false;
                 e.Handled = true;
             }
 
+        }
+
+        private SpeechCore _core;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _core = new SpeechCore();
+            _core.TextChanged += _core_TextChanged;
+            _core.UpdateList += _core_UpdateList;
+
+            new Thread(() =>
+            {
+                _core.Init();
+            }).Start();
+        }
+
+        private void _core_UpdateList(object sender, EventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                listHistory.ItemsSource = _core.AllText;
+                listHistory.Items.Refresh();
+                listHistory.SelectedIndex = listHistory.Items.Count - 1;
+                listHistory.ScrollIntoView(listHistory.SelectedItem);
+                
+            });
+        }
+
+        private void _core_TextChanged(object sender, SpeechEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                txtMain.Text = e.Text.Text;
+            });
+
+        }
+
+        private bool _historyShown = false;
+
+        private void btnShowHistory_Click(object sender, RoutedEventArgs e)
+        {
+            if(_historyShown)
+            {
+                listHistory.Visibility = Visibility.Collapsed;
+                this.Height -= 100;
+            }
+            else
+            {
+                listHistory.Visibility = Visibility.Visible;
+                this.Height += 100;
+            }
+
+            _historyShown = !_historyShown;
+        }
+
+        private void btnPreference_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new ConfigWindow();
+            dialog.Show(); 
         }
     }
 }
---
+++ TMSpeech.GUI/SpeechCore.cs
@@ -0,0 +1,114 @@
+﻿using NAudio.Wave;
+using SherpaOnnx;
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMSpeech.GUI
+{
+
+    public class SpeechEventArgs
+    {
+        public TextInfo Text { get; set; }
+    }
+
+    public class TextInfo
+    {
+        public DateTime Time { get; set; }
+        public string TimeStr => Time.ToString("T");
+        public string Text { get; set; }
+        public TextInfo(string text)
+        {
+            Time = DateTime.Now;
+            Text = text;
+        }
+    }
+
+
+    class SpeechCore
+    {
+        public IList<TextInfo> AllText { get; set; } = new List<TextInfo>();
+        public string CurrentText { get; set; }
+        public event EventHandler<SpeechEventArgs> TextChanged;
+        public event EventHandler<EventArgs> UpdateList;
+
+        public void Clear()
+        {
+            AllText.Clear();
+            CurrentText = "";
+        }
+
+        public void Init()
+        {
+            OnlineRecognizerConfig config = new OnlineRecognizerConfig();
+            config.FeatConfig.SampleRate = 48000;
+            config.FeatConfig.FeatureDim = 80;
+            config.TransducerModelConfig.Encoder = @"D:\models\encoder-epoch-99-avg-1.onnx";
+            config.TransducerModelConfig.Decoder = @"D:\models\decoder-epoch-99-avg-1.onnx";
+            config.TransducerModelConfig.Joiner = @"D:\models\joiner-epoch-99-avg-1.onnx";
+            config.TransducerModelConfig.Tokens = @"D:\models\tokens.txt";
+            config.TransducerModelConfig.NumThreads = 1;
+            config.TransducerModelConfig.Debug = 1;
+
+            config.DecodingMethod = "greedy_search";
+            config.EnableEndpoint = 1;
+
+            config.Rule1MinTrailingSilence = 2.4f;
+            config.Rule2MinTrailingSilence = 1.2f;
+            config.Rule3MinUtteranceLength = 300;
+
+            OnlineRecognizer recognizer = new OnlineRecognizer(config);
+
+            OnlineStream s = recognizer.CreateStream();
+
+            int inputSampleRate = config.FeatConfig.SampleRate;
+            var capture = new WasapiLoopbackCapture();
+            capture.WaveFormat = WaveFormat.CreateIeeeFloatWaveFormat(inputSampleRate, 1);
+            capture.DataAvailable += (_, a) =>
+            {
+                var buffer = new float[a.BytesRecorded / 4];
+                Buffer.BlockCopy(a.Buffer, 0, buffer, 0, a.BytesRecorded);
+                s.AcceptWaveform(inputSampleRate, buffer);
+            };
+            capture.RecordingStopped += (_, a) =>
+            {
+                // TODO: dispose resources.
+            };
+
+            capture.StartRecording();
+
+            while (true)
+            {
+                if (recognizer.IsReady(s))
+                    recognizer.Decode(s);
+                var is_endpoint = recognizer.IsEndpoint(s);
+                var text = recognizer.GetResult(s).Text;
+                System.Diagnostics.Trace.WriteLine(text);
+
+                if (!string.IsNullOrEmpty(text))
+                {
+                    var item = new TextInfo(text);
+                    TextChanged?.Invoke(this, new SpeechEventArgs()
+                    {
+                        Text = item,
+                    });
+                    CurrentText = text;
+
+                    if (is_endpoint || text.Length >= 80)
+                    {
+                        AllText.Add(item);
+                        recognizer.Reset(s);
+                        UpdateList?.Invoke(this, EventArgs.Empty);
+                    }
+
+                }
+
+
+            }
+        }
+
+    }
+}
