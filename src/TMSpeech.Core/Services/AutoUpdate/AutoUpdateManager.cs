4a95ee7b2cc96c6fbfbca84e4b8619c77bad7f2d
程子丘 <jxlpzqc@live.com>
2024-11-01 19:10:39 +0800

    feat: skeleton of auto update manager

---
+++ src/TMSpeech.Core/Services/AutoUpdate/AutoUpdateManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMSpeech.Core.AutoUpdate;
+
+class UpdateInfo
+{
+    public long Version { get; set; }
+    public string NewFeature { get; set; }
+}
+
+class AutoUpdateManager
+{
+    public UpdateInfo? CheckUpdate()
+    {
+        // TODO: implement it
+        return null;
+    }
+}
