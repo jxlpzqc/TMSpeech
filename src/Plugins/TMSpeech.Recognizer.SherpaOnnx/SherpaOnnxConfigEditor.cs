aa17f39f3b73ade659e3225d836f376fdb08444a
程子丘 <jxlpzqc@live.com>
2024-11-03 14:56:19 +0800

    feat(plugin): support select download module

--- src/Plugins/TMSpeech.Recognizer.SherpaOnnx/SherpaOnnxConfigEditor.cs
+++ src/Plugins/TMSpeech.Recognizer.SherpaOnnx/SherpaOnnxConfigEditor.cs
@@ -13,7 +13,7 @@ namespace TMSpeech.Recognizer.SherpaOnnx
     class SherpaOnnxConfig
     {
         [JsonPropertyName("model")]
-        public string Model { get; set; } = "models";
+        public string Model { get; set; } = "";
 
         [JsonPropertyName("encoder")]
         public string Encoder { get; set; } = "models\\encoder.onnx";
@@ -34,7 +34,12 @@ namespace TMSpeech.Recognizer.SherpaOnnx
 
         public void SetValue(string key, object value)
         {
-            if (key == "model") _config.Model = (string)value;
+            if (key == "model")
+            {
+                _config.Model = (string)value;
+                FormItemsUpdated?.Invoke(this, EventArgs.Empty);
+            }
+
             if (key == "encoder") _config.Encoder = (string)value;
             if (key == "decoder") _config.Decoder = (string)value;
             if (key == "joiner") _config.Joiner = (string)value;
@@ -53,9 +58,28 @@ namespace TMSpeech.Recognizer.SherpaOnnx
 
         public IReadOnlyList<PluginConfigFormItem> GetFormItems()
         {
+            var models = Core.Services.Resource.ResourceManagerFactory.Instance.GetLocalModuleInfos().Result
+                .Where(u => u.Type == Core.Services.Resource.ModuleInfoTypeEnums.SherpaOnnxModel);
+
+            var options = new Dictionary<object, string>
+            {
+                { "", "自定义模型" },
+            }.Union(
+                models.Select(x => new KeyValuePair<object, string>(x.ID,
+                    $"{x.Name} ({x.ID})"))
+            ).ToDictionary(x => x.Key, x => x.Value);
+
+            if (!string.IsNullOrEmpty(_config.Model))
+            {
+                return new PluginConfigFormItem[]
+                {
+                    new PluginConfigFormItemOption("model", "模型", options)
+                };
+            }
+
             return new PluginConfigFormItem[]
             {
-                // new PluginConfigFormItemText("model", "模型"),
+                new PluginConfigFormItemOption("model", "模型", options),
                 new PluginConfigFormItemFile("encoder", "编码器"),
                 new PluginConfigFormItemFile("decoder", "解码器"),
                 new PluginConfigFormItemFile("joiner", "连接器"),
