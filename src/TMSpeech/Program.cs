3198ba9862774c40262457d99bbb4f83a6ac26c1
am009 <warrenwjk@qq.com>
2025-12-09 00:01:22 +0800

    增加日志文件LastLog.log

--- src/TMSpeech/Program.cs
+++ src/TMSpeech/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 using Avalonia;
 using Avalonia.ReactiveUI;
 using DesktopNotifications;
@@ -9,6 +11,7 @@ namespace TMSpeech.GUI.Desktop;
 
 class Program
 {
+    public const string LogFileName = "LastRun.log";
     internal static INotificationManager NotificationManager = null!;
 
     // Initialization code. Don't use any Avalonia, third-party APIs or any
@@ -19,14 +22,23 @@ class Program
     {
         try
         {
+            Trace.Listeners.Add(new TextWriterTraceListener(LogFileName));
+        }
+        catch (Exception e)
+        {
+            Debug.WriteLine($"打开日志文件失败！: {e.Message}");
+        }
+
+        try
+        {
             Initializer.InitialzeServices();
             BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
         }
         catch (Exception e)
         {
-            System.Diagnostics.Debug.WriteLine($"主线程 unhandled exception: {e.Message}");
+            Debug.WriteLine($"主线程 unhandled exception: {e.Message}");
             // TODO 当前窗口会卡死，等待10秒展示报错后退出。如何防止卡死？
-            MessageBoxManager.GetMessageBoxStandard("严重错误", $"未捕获的异常：\n{e.Message}\n{e.StackTrace}\n请将问题截图提交到Github Issue，10s后应用将退出").ShowAsync().Wait(10000);
+            MessageBoxManager.GetMessageBoxStandard("严重错误", $"未捕获的异常：\n{e.Message}\n{e.StackTrace}\n请将日志提交到Github Issue，10s后应用将退出\n日志文件位置：{Directory.GetCurrentDirectory()}\\{LogFileName}").ShowAsync().Wait(10000);
         }
     }
 
