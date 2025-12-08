687009d9cdcc4eb41c61e2b3c05b32f925190515
am009 <warrenwjk@qq.com>
2025-12-08 23:27:43 +0800

    捕获主线程异常并展示

--- src/TMSpeech/Program.cs
+++ src/TMSpeech/Program.cs
@@ -2,6 +2,7 @@
 using Avalonia;
 using Avalonia.ReactiveUI;
 using DesktopNotifications;
+using MsBox.Avalonia;
 using TMSpeech.Services;
 
 namespace TMSpeech.GUI.Desktop;
@@ -16,8 +17,17 @@ class Program
     [STAThread]
     public static void Main(string[] args)
     {
-        Initializer.InitialzeServices();
-        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        try
+        {
+            Initializer.InitialzeServices();
+            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        }
+        catch (Exception e)
+        {
+            System.Diagnostics.Debug.WriteLine($"主线程 unhandled exception: {e.Message}");
+            // TODO 当前窗口会卡死，等待10秒展示报错后退出。如何防止卡死？
+            MessageBoxManager.GetMessageBoxStandard("严重错误", $"未捕获的异常：\n{e.Message}\n{e.StackTrace}\n请将问题截图提交到Github Issue，10s后应用将退出").ShowAsync().Wait(10000);
+        }
     }
 
     // Avalonia configuration, don't remove; also used by visual designer.
